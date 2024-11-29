3bcb8b0a3e9859ba01ca1427cc025ad83a065f34
cattyn <cvenby@gmail.com>
2024-11-29 15:21:28 +0300

    added support for multiple params in eventconsumer

---
+++ src/main/java/bot/staro/rokit/utils/ReflectionUtil.java
@@ -0,0 +1,25 @@
+package bot.staro.rokit.utils;
+
+import java.util.Map;
+
+public final class ReflectionUtil {
+    public static final Map<Class<?>, Class<?>> PRIMITIVES = Map.of(
+            int.class, Integer.class,
+            byte.class, Byte.class,
+            short.class, Short.class,
+            long.class, Long.class,
+            boolean.class, Boolean.class,
+            float.class, Float.class,
+            double.class, Double.class,
+            char.class, Character.class
+    );
+
+    private ReflectionUtil() {
+        throw new AssertionError();
+    }
+
+    public static Class<?> toNonPrimitive(Class<?> klass) {
+        if (!klass.isPrimitive()) return klass;
+        return PRIMITIVES.get(klass);
+    }
+}
