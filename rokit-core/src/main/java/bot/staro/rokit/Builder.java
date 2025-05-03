eeacab9eac41922a4dc7aa08dc1d40653117e554
selfpop <wesley_jr@cryptolab.net>
2025-05-03 16:24:45 +0200

    2.0.9 - fixed the builder's visibility

--- rokit-core/src/main/java/bot/staro/rokit/Builder.java
+++ rokit-core/src/main/java/bot/staro/rokit/Builder.java
@@ -3,20 +3,23 @@ package bot.staro.rokit;
 import java.util.HashMap;
 import java.util.Map;
 
-public final class Builder {
-    final Map<Class<?>, EventWrapper<?>> wrappers = new HashMap<>();
+public class Builder {
+    protected final Map<Class<?>, EventWrapper<?>> wrappers = new HashMap<>();
 
-    <T> Builder wrap(Class<T> eventType, EventWrapper<T> wrapper) {
+    protected Builder() {
+    }
+
+    public <T> Builder wrap(Class<T> eventType, EventWrapper<T> wrapper) {
         wrappers.put(eventType, wrapper);
         return this;
     }
 
-    <T> Builder wrap(Class<T> eventType, SingleEventWrapper<T> wrapper) {
+    public <T> Builder wrap(Class<T> eventType, SingleEventWrapper<T> wrapper) {
         wrappers.put(eventType, wrapper);
         return this;
     }
 
-    RokitEventBus build() {
+    public RokitEventBus build() {
         RokitEventBus bus = new RokitEventBus();
         bus.wrappers.putAll(wrappers);
         return bus;
